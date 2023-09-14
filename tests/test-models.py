7f3431a122c2b7b09e8d8d03f612aa39fb66ad50
MG <meherzad.gotla@gmail.com>
2023-09-13 23:03:55 -0700

    With tests

---
+++ tests/test-models.py
@@ -0,0 +1,7 @@
+from django.tests import TestCase
+from restaurant.models import Booking, Menu
+
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title="IceCream", price=80, inventory=100)
+        self.assertEqual(item, "IceCream : 80")
